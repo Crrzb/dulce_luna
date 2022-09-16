da16129b367c512fa0c11ad9d51f881374fbafe3
Alejandro Carranza <crrzb@hotmail.com>
2022-09-16 15:08:18 -0500

    Agregue todos los archivos locales al repositorio vacio

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+let menu = document.getElementById("navbarSupportedContent");
+let toggler_menu = document.getElementById("toggler_menu");
+let toggler_close = document.getElementById("toggler_close");
+function navClick() {
+    
+    menu.classList.toggle("visually-hidden");
+    toggler_menu.classList.toggle("visually-hidden");
+    toggler_close.classList.toggle("visually-hidden");
+}
