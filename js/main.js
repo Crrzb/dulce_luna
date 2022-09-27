ffebb063ef7645f1c4fe89b4b555eaa0172f6c94
Alejandro Carranza <crrzb@hotmail.com>
2022-09-26 20:46:19 -0500

    inicio actualizado, menu.html, fotos.html

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 
-let menu = document.getElementById("navbarSupportedContent");
-let toggler_menu = document.getElementById("toggler_menu");
-let toggler_close = document.getElementById("toggler_close");
-function navClick() {
-    
-    menu.classList.toggle("visually-hidden");
-    toggler_menu.classList.toggle("visually-hidden");
-    toggler_close.classList.toggle("visually-hidden");
+
+let dropdown_menu = document.getElementById("dropdown_menu");
+let icon_menu = document.getElementById("icon_menu");
+
+
+function click_menu(){
+    dropdown_menu.classList.toggle("visually-hidden");
+
+    icon_menu.classList.toggle("fa-sharp");
+    icon_menu.classList.toggle("fa-solid");
+    icon_menu.classList.toggle("fa-2x");
+    icon_menu.classList.toggle("fa-bars");
+
+    icon_menu.classList.toggle("fa-solid");
+    icon_menu.classList.toggle("fa-xmark");
+    icon_menu.classList.toggle("fa-2x");
 }
